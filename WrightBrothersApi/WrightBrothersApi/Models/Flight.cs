9f0cf0daa62637ea76924ee3703dc660a3db9ef6
Thijs Limmen <limmenthijs@gmail.com>
2024-02-05 13:09:19 +0100

    remove changes

--- WrightBrothersApi/WrightBrothersApi/Models/Flight.cs
+++ WrightBrothersApi/WrightBrothersApi/Models/Flight.cs
@@ -31,8 +31,6 @@ public class Flight
     // Flight number WB001
     public string FlightLogSignature { get; set; }
 
-    public FlightLog FlightLog { get; set; }
-
     // Encodes a series of aerobatic maneuvers
     // L = Loop, H = Hammerhead, R = Roll, S = Spin, T = Tailslide
     // Number represents repeat count, Letter represents difficulty (A-E), and each has a unique ID
--- WrightBrothersApi/WrightBrothersApi/Models/FlightLogSignature.cs
+++
@@ -1,8 +0,0 @@
-public class FlightLog
-{
-    public string Date { get; set; }
-    public string Departure { get; set; }
-    public string Arrival { get; set; }
-    public string FlightNumber { get; set; }
-}
-
