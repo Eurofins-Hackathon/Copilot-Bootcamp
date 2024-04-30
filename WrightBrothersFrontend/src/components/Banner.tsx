c739eb608cf64d979278635145c7061729b8adaa
Thijs Limmen <limmenthijs@gmail.com>
2024-04-30 11:08:42 +0200

    uncenter banner

--- WrightBrothersFrontend/src/components/Banner.tsx
+++ WrightBrothersFrontend/src/components/Banner.tsx
@@ -3,7 +3,7 @@ import { Airplane } from "./Airplane"; // Ensure this has a vintage style
 
 const Banner: React.FC = () => {
   return (
-    <div className="relative vintage-filter bg-amber-600 h-1/2 overflow-hidden py-32 flex items-center justify-center">
+    <div className="relative vintage-filter bg-amber-600 h-1/2 overflow-hidden py-32">
       <div className="z-10 px-4 sm:px-6 lg:px-8 max-w-screen-md flex flex-col text-center">
         {/* Increased spacing around text */}
         <h1 className="text-5xl leading-none font-bold text-amber-100 sm:text-6xl sm:leading-tight">
