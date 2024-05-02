8c34479f084de7a69252c18a333937ec8e0ae819
Thijs Limmen <limmenthijs@gmail.com>
2024-05-02 16:09:26 +0200

    improvements

--- WrightBrothersFrontend/src/pages/HomePage.tsx
+++ WrightBrothersFrontend/src/pages/HomePage.tsx
@@ -1,31 +1,19 @@
-import React from 'react';
-import axios from 'axios';
-import { useQuery } from 'react-query';
 import Banner from "../components/Banner";
 import PlaneList from "../components/PlaneList";
 import PageContent from "../components/PageContent";
-import Spinner from "../components/Spinner"; // Assuming you have a Spinner component
-
-async function fetchPlanes() {
-  await new Promise(resolve => setTimeout(resolve, 3000)); // Add a delay of 3 seconds
-  const response = await axios.get('http://localhost:1903/planes/');
-  return response.data;
-}
 
 function HomePage() {
-  const { isLoading, isError, data: planes } = useQuery('planes', fetchPlanes);
+  const planes = [
+    { id: 1, name: "Wright Flyer I" },
+    { id: 2, name: "Wright Flyer II" },
+    { id: 3, name: "Wright Model A" },
+  ];
 
   return (
     <div>
       <Banner />
       <PageContent>
-        {isLoading ? (
-          <Spinner /> // Display Spinner when loading
-        ) : isError ? (
-          <div>There was an error!</div> // Display error message on error
-        ) : (
-          <PlaneList planes={planes} />
-        )}
+        <PlaneList planes={planes} />
       </PageContent>
     </div>
   );
