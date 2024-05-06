63b97389a642772c77f9ca97ce0415bb91e8dbb0
Thijs Limmen <limmenthijs@gmail.com>
2024-05-06 15:27:50 +0200

    reset homepage

--- WrightBrothersFrontend/src/App.tsx
+++ WrightBrothersFrontend/src/App.tsx
@@ -1,5 +1,3 @@
-// App.tsx
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage"; // Import your page components
--- WrightBrothersFrontend/src/pages/HomePage.tsx
+++ WrightBrothersFrontend/src/pages/HomePage.tsx
@@ -1,28 +1,18 @@
-import axios from 'axios';
-import { useQuery } from 'react-query';
 import Banner from "../components/Banner";
 import PlaneList from "../components/PlaneList";
 import PageContent from "../components/PageContent";
-import PlaneSpinner from "../components/PlaneSpinner";
-
-const fetchPlanes = async () => {
-  await new Promise(resolve => setTimeout(resolve, 3000)); // Add delay
-  const response = await axios.get('http://localhost:1903/planes/');
-  return response.data;
-};
 
 function HomePage() {
-  const { isLoading, isSuccess, isError, data: planes } = useQuery('planes', fetchPlanes);
+  const planes = [
+    { id: 1, name: "Local Plane 1" },
+    { id: 2, name: "Local Plane 2" },
+  ];
 
   return (
     <div>
       <Banner />
       <PageContent>
-        {isLoading || isError ? (
-          <PlaneSpinner isLoading={isLoading} isError={isError} isSuccess={isSuccess} />
-        ) : (
-          <PlaneList planes={planes} />
-        )}
+        <PlaneList planes={planes} />
       </PageContent>
     </div>
   );
